0def50fa158aeda00f965d40443aea7611aca8ed
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-12 16:36:23 -0600

    Add Settings Tabs

--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -10,7 +10,7 @@ export function UsedSpaceWidget() {
         </p>
       </div>
 
-      <div className="h-2 rounded-full bg-violet-100">
+      <div className="h-2 rounded-full bg-violet-200">
         <div className="h-2 w-4/5 rounded-full bg-violet-600"></div>
       </div>
 
