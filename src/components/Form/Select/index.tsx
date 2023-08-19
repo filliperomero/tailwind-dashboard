95a7db14a45214a7864b7c31611f565b782739a4
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-18 23:41:12 -0600

    Remove default outline and add correct focus style

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -11,7 +11,7 @@ export interface SelectProps extends RadixSelect.SelectProps {
 export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <RadixSelect.Root {...props}>
-      <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
+      <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
         <RadixSelect.Value placeholder={placeholder} className="text-black" />
         <RadixSelect.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
