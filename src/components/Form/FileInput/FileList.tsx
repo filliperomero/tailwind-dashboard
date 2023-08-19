623f52f90c7d73eb95cf1a22c8d1951da8cf7093
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-19 14:44:16 -0600

    Add random state to FileList so we can test all 3 states

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -13,14 +13,25 @@ export function FileList(props: FileListProps) {
 
   return (
     <div ref={parent} className="mt-4 space-y-3" {...props}>
-      {files.map((file, index) => (
-        <FileItem
-          key={index}
-          name={file.name}
-          size={file.size}
-          state="complete"
-        />
-      ))}
+      {files.map((file, index) => {
+        // For tests purposes (min = 1; max = 3)
+        const randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1)
+
+        return (
+          <FileItem
+            key={index}
+            name={file.name}
+            size={file.size}
+            state={
+              randomNumber === 1
+                ? 'progress'
+                : randomNumber === 2
+                ? 'complete'
+                : 'error'
+            }
+          />
+        )
+      })}
     </div>
   )
 }
